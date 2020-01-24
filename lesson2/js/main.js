0a7867cca1a5ee764bfab07a97b07d5819ab7bac
Elenandreeva <confeta@bk.ru>
2020-01-24 12:11:10 +0300

    ++

---
+++ lesson2/js/main.js
@@ -0,0 +1,23 @@
+let money = 45000, 
+    income = 10000,
+    addExpenses = "internet, taxi, utilityExpenses",
+    deposit = false,
+    mission = 100000,
+    period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log("Период равен " + period + " месяцев. Цель заработать " + mission + " рублей.");
+
+console.log(addExpenses.toLowerCase().split());
+
+let budgetDay = money/30;
+console.log(budgetDay);
+
+
+
+
