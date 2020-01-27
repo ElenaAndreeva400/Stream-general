35daca2152aaf618fe900d198928a5350d34f07e
Elenandreeva <confeta@bk.ru>
2020-01-27 12:00:00 +0300

    ++

---
+++ lesson3/js/main.js
@@ -0,0 +1,51 @@
+'use strict'; // эта директива появилась в ES5
+
+let money = 45000, 
+    income = 10000,
+    addExpenses= [],
+    deposit = false,
+    mission = 100000,
+    period = 12;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+
+money = prompt('Ваш месячный доход?', 45000);
+money = parseInt(money);
+addExpenses.push(prompt('Перечислите возможные расходы за рассчитываемый период через запятую?', "еда, связь").toLowerCase().split(', '));
+console.log(addExpenses.length);
+console.log(addExpenses);
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+let expenses1 = prompt('Введите первую обязательную статью расходов?', 'еда').toLowerCase();
+let amount1 = parseInt(prompt('Во сколько это обойдется?', 20000));
+let expenses2 = prompt('Введите вторую обязательную статью расходов?', 'связь').toLowerCase();
+let amount2 = parseInt(prompt('Во сколько это обойдется?', 400));
+
+let budgetMonth = (money + income) - amount1 - amount2; // учитывая обязательные расходы
+console.log(`Бюджет на месяц ${budgetMonth}`);
+
+period = Math.ceil((mission/budgetMonth));
+console.log(`Цель будет достигнута за ${period} месяца.`);
+
+console.log("Период равен " + period + " месяцев. Цель заработать " + mission + " рублей.");
+
+let budgetDay = Math.floor(budgetMonth/30);
+console.log(`Бюджет на день ${budgetDay}`);
+
+if (budgetDay<0) {
+    console.log('Что-то пошло не так');
+} else if (budgetDay<600) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay>1200) {
+    console.log('У Вас высокий уровень дохода');
+} else if (budgetDay>600) {
+    console.log('У вас средний уровень дохода');
+} else { 
+    console.log('Необходимо ввести положительное число');
+}
+
+
+
+
